cf988cf8389d5af6349ba834a434ad31649247fc
GutorovMikhail <mgutorov@phoenixit.ru>
2023-12-01 19:40:32 +0300

    День 1. Требушет. Часть 2.

--- src/main/kotlin/twenty_three/one/Main.kt
+++ src/main/kotlin/twenty_three/one/Main.kt
@@ -4,27 +4,73 @@ import java.io.File
 
 fun main() {
     val strings: List<String> = File("src/main/resources/2023_day_1_input.txt").bufferedReader().readLines()
-    var sum: Long = 0
-    var number: StringBuilder = java.lang.StringBuilder()
-    strings.forEach {
-        for (i in it.indices) {
-            val element: Char = it[i]
-            if (element.isDigit()) {
-                number.append(element)
-                break
-            }
-        }
-        for (i in it.length - 1 downTo  0){
-            val element: Char = it[i]
-            if (element.isDigit()) {
-                number.append(element)
-                break
-            }
-        }
-        sum += number.toString().toLong()
-        number = java.lang.StringBuilder()
-    }
+    val sum: Long = strings.stream().mapToLong {
+        replaceNumbers(it)
+    }.sum()
 
     println(sum)
 
+}
+
+fun replaceNumbers(input: String): Long {
+    val result: StringBuilder = java.lang.StringBuilder()
+    result.append(findFirstNumber(input))
+    result.append(findLastNumber(input))
+    return result.toString().toLong()
+}
+
+fun findFirstNumber(input: String): Char {
+    for (i in input.indices) {
+        val element: Char = input[i]
+        if (element.isDigit()) {
+            return element
+        } else if (element == 'o' && i <= input.length - 3 && input[i + 1] == 'n' && input[i + 2] == 'e') {
+            return '1'
+        } else if (element == 't' && i <= input.length - 3 && input[i + 1] == 'w' && input[i + 2] == 'o') {
+            return '2'
+        } else if (element == 't' && i <= input.length - 5 && input[i + 1] == 'h' && input[i + 2] == 'r' && input[i + 3] == 'e' && input[i + 4] == 'e') {
+            return '3'
+        } else if (element == 'f' && i <= input.length - 4 && input[i + 1] == 'o' && input[i + 2] == 'u' && input[i + 3] == 'r') {
+            return '4'
+        } else if (element == 'f' && i <= input.length - 4 && input[i + 1] == 'i' && input[i + 2] == 'v' && input[i + 3] == 'e') {
+            return '5'
+        } else if (element == 's' && i <= input.length - 3 && input[i + 1] == 'i' && input[i + 2] == 'x') {
+            return '6'
+        } else if (element == 's' && i <= input.length - 5 && input[i + 1] == 'e' && input[i + 2] == 'v' && input[i + 3] == 'e' && input[i + 4] == 'n') {
+            return '7'
+        } else if (element == 'e' && i <= input.length - 5 && input[i + 1] == 'i' && input[i + 2] == 'g' && input[i + 3] == 'h' && input[i + 4] == 't') {
+            return '8'
+        } else if (element == 'n' && i <= input.length - 4 && input[i + 1] == 'i' && input[i + 2] == 'n' && input[i + 3] == 'e') {
+            return '9'
+        }
+    }
+    return ' '
+}
+
+fun findLastNumber(input: String): Char {
+    for (i in input.length - 1 downTo 0) {
+        val element: Char = input[i]
+        if (element.isDigit()) {
+            return element
+        } else if (element == 'e' && i >= 3 && input[i - 1] == 'n' && input[i - 2] == 'o') {
+            return '1'
+        } else if (element == 'o' && i >= 3 && input[i - 1] == 'w' && input[i - 2] == 't') {
+            return '2'
+        } else if (element == 'e' && i >= 5 && input[i - 1] == 'e' && input[i - 2] == 'r' && input[i - 3] == 'h' && input[i - 4] == 't') {
+            return '3'
+        } else if (element == 'r' && i >= 4 && input[i - 1] == 'u' && input[i - 2] == 'o' && input[i - 3] == 'f') {
+            return '4'
+        } else if (element == 'e' && i >= 4 && input[i - 1] == 'v' && input[i - 2] == 'i' && input[i - 3] == 'f') {
+            return '5'
+        } else if (element == 'x' && i >= 3 && input[i - 1] == 'i' && input[i - 2] == 's') {
+            return '6'
+        } else if (element == 'n' && i >= 5 && input[i - 1] == 'e' && input[i - 2] == 'v' && input[i - 3] == 'e' && input[i - 4] == 's') {
+            return '7'
+        } else if (element == 't' && i >= 5 && input[i - 1] == 'h' && input[i - 2] == 'g' && input[i - 3] == 'i' && input[i - 4] == 'e') {
+            return '8'
+        } else if (element == 'e' && i >= 4 && input[i - 1] == 'n' && input[i - 2] == 'i' && input[i - 3] == 'n') {
+            return '9'
+        }
+    }
+    return ' '
 }
