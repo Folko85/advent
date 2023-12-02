da2fb0853349a85239a60c469aa8f36e8674c68a
GutorovMikhail <mgutorov@phoenixit.ru>
2023-12-02 12:26:32 +0300

    День 2. Головоломка с кубиками. Часть 2

--- src/main/kotlin/year2023/Day02.kt
+++ src/main/kotlin/year2023/Day02.kt
@@ -29,6 +29,28 @@ fun main() {
         return if (isPossible) gameNumber else 0
     }
 
+    fun findPower(input: String): Long {
+        var maxRedCubesLocal: Long = 0
+        var maxGreenCubesLocal: Long = 0
+        var maxBlueCubesLocal: Long = 0
+        val game: List<String> = input.split(":")
+        val rounds: List<String> = game[1].split(";")
+        rounds.forEach { round ->
+            val colors: List<String> = round.trim().split(",")
+            colors.forEach { color ->
+                val quantity: Long = color.filter { it.isDigit() }.toLong()
+                if (color.contains("red")) {
+                    maxRedCubesLocal = if (quantity > maxRedCubesLocal) quantity else maxRedCubesLocal
+                } else if (color.contains("blue")) {
+                    maxBlueCubesLocal = if (quantity > maxBlueCubesLocal) quantity else maxBlueCubesLocal
+                } else if (color.contains("green")) {
+                    maxGreenCubesLocal = if (quantity > maxGreenCubesLocal) quantity else maxGreenCubesLocal
+                }
+            }
+        }
+        return maxBlueCubesLocal * maxGreenCubesLocal * maxRedCubesLocal
+    }
+
     fun part1() {
         var sum: Long = 0
         val strings: List<String> = File("src/main/resources/2023_day_2_input.txt").bufferedReader().readLines()
@@ -39,13 +61,18 @@ fun main() {
         println(sum)
     }
 
-//    fun part2() {
-//        val strings: List<String> = File("src/main/resources/2023_day_2_example2.txt").bufferedReader().readLines()
-//
-//    }
+    fun part2() {
+        var sum: Long = 0
+        val strings: List<String> = File("src/main/resources/2023_day_2_input.txt").bufferedReader().readLines()
+        strings.forEach {
+            val value: Long = findPower(it)
+            sum += value
+        }
+        println(sum)
+    }
 
     part1()
-//    part2()
+    part2()
 
 
 }
