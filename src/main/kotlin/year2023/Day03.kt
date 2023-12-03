cbff10b817d35c63ecd04e961569b20aa8c11b80
GutorovMikhail <mgutorov@phoenixit.ru>
2023-12-03 15:00:45 +0300

    День 3. Передаточные числа. Часть 2

--- src/main/kotlin/year2023/Day03.kt
+++ src/main/kotlin/year2023/Day03.kt
@@ -73,7 +73,7 @@ fun main() {
                 }
 
             }
-            if (number.isNotEmpty() && accepted){
+            if (number.isNotEmpty() && accepted) {
                 sum += number.toString().toLong()
             }
             number.clear()
@@ -84,13 +84,121 @@ fun main() {
         println(sum)
     }
 
-//    fun part2() {
-//        var sum: Long = 0
-//        val strings: List<String> = File("src/main/resources/2023_day_3_example.txt").bufferedReader().readLines()
-//    }
+    fun checkAdjacentGears(j: Int, k: Int, array: Array<CharArray>): String {
+        var tempSymbol: Char
+        if (j > 0 && k > 0) {
+            tempSymbol = array[j - 1][k - 1]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j - 1, k - 1)
+            }
+        }
+        if (j > 0 && k < array[0].size - 1) {
+            tempSymbol = array[j - 1][k + 1]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j - 1, k + 1)
+            }
+        }
+        if (j < array.size - 1 && k > 0) {
+            tempSymbol = array[j + 1][k - 1]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j + 1, k - 1)
+            }
+        }
+        if (j > 0) {
+            tempSymbol = array[j - 1][k]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j - 1, k)
+            }
+        }
+        if (k > 0) {
+            tempSymbol = array[j][k - 1]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j, k - 1)
+            }
+        }
+        if (j < array.size - 1 && k < array[0].size - 1) {
+            tempSymbol = array[j + 1][k + 1]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j + 1, k + 1)
+            }
+        }
+        if (j < array.size - 1) {
+            tempSymbol = array[j + 1][k]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j + 1, k)
+            }
+        }
+        if (k < array[0].size - 1) {
+            tempSymbol = array[j][k + 1]
+            if (tempSymbol == '*') {
+                return String.format("%d_%d", j, k + 1)
+            }
+        }
+        return "*_*"
+    }
+
+    fun part2() {
+        var sum: Long = 0
+        val strings: List<String> = File("src/main/resources/2023_day_3_input.txt").bufferedReader().readLines()
+        val rows: Int = strings.size
+        val cols: Int = strings[0].length
+        val array = Array(rows) { CharArray(cols) }
+        for (i in 0..<rows) {
+            array[i] = strings[i].toCharArray()
+        }
+
+        var accepted = false
+        val gears: MutableMap<String, Long> = mutableMapOf()
+        var gear = "*_*"
+        for (j in 0..<rows) {
+            val number: StringBuilder = java.lang.StringBuilder()
+            for (k in 0..<cols) {
+                val symbol: Char = array[j][k]
+                if (symbol.isDigit()) {
+                    number.append(symbol)
+                    if (!accepted) {
+                        gear = checkAdjacentGears(j, k, array)
+                        if (gear != "*_*") {
+                            accepted = true
+                        }
+
+                    }
+                } else if (!symbol.isDigit()) {
+                    if (number.isNotEmpty() && accepted) {
+                        if (gears.containsKey(gear)) {
+                            val first: Long = gears[gear]!!
+                            sum += first * number.toString().toLong()
+                            gears.remove(gear)
+                        } else {
+                            gears[gear] = number.toString().toLong()
+                        }
+
+                    }
+                    gear = "*_*"
+                    number.clear()
+                    accepted = false
+                }
+
+            }
+            if (number.isNotEmpty() && accepted) {
+                if (gears.containsKey(gear)) {
+                    val first: Long = gears[gear]!!
+                    sum += first * number.toString().toLong()
+                    gears.remove(gear)
+                } else {
+                    gears[gear] = number.toString().toLong()
+                }
+            }
+            gear = "*_*"
+            number.clear()
+            accepted = false
+        }
+
+        println(sum)
+    }
 
     part1()
-//    part2()
+    part2()
 
 
 }
