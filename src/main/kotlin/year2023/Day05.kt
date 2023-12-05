041b8dc6d8d0990b251d7de06ab6ca353a310d85
GutorovMikhail <mgutorov@phoenixit.ru>
2023-12-05 20:25:25 +0300

    День 5. Огород. Часть 2.

--- src/main/kotlin/year2023/Day05.kt
+++ src/main/kotlin/year2023/Day05.kt
@@ -6,22 +6,12 @@ fun main() {
 
     fun findNext(listOfPair: MutableList<Pair<LongProgression, LongProgression>>, sourceIndex: Long): Long {
         var resultIndex: Long = -1L
-        listOfPair.forEach { progressionPair ->
-            if (progressionPair.first.contains(sourceIndex)) {
-                var counter: Long = 0L
-                for (i in progressionPair.first.first..progressionPair.first.last) {
-                    if (i == sourceIndex) {
-                        break
-                    }
-                    counter++
-                }
-                for (j in progressionPair.second.first..progressionPair.second.last) {
-                    if (counter == 0L) {
-                        resultIndex = j
-                        break
-                    }
-                    counter--
-                }
+        for (i in listOfPair.indices) {
+            val progressionPair = listOfPair[i]
+            if (progressionPair.first.first <= sourceIndex && sourceIndex <= progressionPair.first.last) {
+                val counter = sourceIndex - progressionPair.first.first
+                resultIndex = progressionPair.second.first + counter
+
             }
         }
         if (resultIndex == -1L) {
@@ -35,13 +25,13 @@ fun main() {
         var lowestSeed: Long = Long.MAX_VALUE
         var seeds: List<Long> = listOf()
         var currentMap: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
-        var seedToSoil: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
-        var soilToFertilizer: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
-        var fertilizerToWater: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
-        var waterToLight: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
-        var lightToTemperature: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
-        var temperatureToHumidity: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
-        var humidityToLocation: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val seedToSoil: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val soilToFertilizer: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val fertilizerToWater: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val waterToLight: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val lightToTemperature: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val temperatureToHumidity: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val humidityToLocation: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
         strings.forEach { string ->
             if (string.contains("seeds")) {
                 seeds = string.split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
@@ -84,11 +74,73 @@ fun main() {
     }
 
     fun part2() {
+        val strings: List<String> = File("src/main/resources/2023_day_5_input.txt").bufferedReader().readLines()
+        var lowestSeed: Long = Long.MAX_VALUE
+        val seeds: MutableList<LongProgression> = mutableListOf()
+        var currentMap: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val seedToSoil: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val soilToFertilizer: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val fertilizerToWater: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val waterToLight: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val lightToTemperature: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val temperatureToHumidity: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        val humidityToLocation: MutableList<Pair<LongProgression, LongProgression>> = mutableListOf()
+        strings.forEach { string ->
+            if (string.contains("seeds")) {
+                val seedsData = string.split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
+                var startSeed: Long = 0
+                var period: Long
+                for (i in seedsData.indices) {
+                    if (i % 2 == 0) {
+                        startSeed = seedsData[i]
+                    } else {
+                        period = seedsData[i]
+                        val progression: LongProgression = startSeed..<startSeed + period
+                        seeds.add(progression)
+                    }
+                }
 
+            } else if (string.contains("seed-to-soil")) {
+                currentMap = seedToSoil
+            } else if (string.contains("soil-to-fertilizer")) {
+                currentMap = soilToFertilizer
+            } else if (string.contains("fertilizer-to-water")) {
+                currentMap = fertilizerToWater
+            } else if (string.contains("water-to-light")) {
+                currentMap = waterToLight
+            } else if (string.contains("light-to-temperature")) {
+                currentMap = lightToTemperature
+            } else if (string.contains("temperature-to-humidity")) {
+                currentMap = temperatureToHumidity
+            } else if (string.contains("humidity-to-location")) {
+                currentMap = humidityToLocation
+            } else if (string.isNotBlank()) {
+                val progressionData: List<String> = string.trim().split("\\s+".toRegex())
+                val sourceStart = progressionData[1].toLong()
+                val targetStart = progressionData[0].toLong()
+                val length = progressionData[2].toLong()
+                val sourceProgression: LongProgression = sourceStart..<sourceStart + length
+                val targetProgression: LongProgression = targetStart..<targetStart + length
+                currentMap.add(Pair(sourceProgression, targetProgression))
+            }
+        }
+        seeds.forEach { seed ->
+            for (i in seed.first..seed.last) {
+                val soilIndex: Long = findNext(seedToSoil, i)
+                val fertilizerIndex: Long = findNext(soilToFertilizer, soilIndex)
+                val waterIndex: Long = findNext(fertilizerToWater, fertilizerIndex)
+                val lightIndex: Long = findNext(waterToLight, waterIndex)
+                val temperatureIndex: Long = findNext(lightToTemperature, lightIndex)
+                val humidityIndex: Long = findNext(temperatureToHumidity, temperatureIndex)
+                val location: Long = findNext(humidityToLocation, humidityIndex)
+                lowestSeed = if (lowestSeed < location) lowestSeed else location
+            }
+        }
+        println(lowestSeed)
     }
 
     part1()
-//    part2()
+    part2()
 
 }
 
