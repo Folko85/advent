0f4c59336cc840b636fa367a42d8785250e6414f
GutorovMikhail <mgutorov@phoenixit.ru>
2023-12-10 13:39:29 +0300

    День 9. Арифметическая прогрессия в Оазисе. Часть 1.

---
+++ src/main/kotlin/year2023/Day09.kt
@@ -0,0 +1,58 @@
+package year2023
+
+import java.io.File
+
+fun main() {
+
+
+    fun calculate(input: String): Long {
+        val list: MutableList<Long> = input.split("\\s+".toRegex()).map {
+            it.trim().toLong()
+        }.toMutableList()
+        val matrix: MutableList<MutableList<Long>> = mutableListOf()
+        matrix.add(list)
+        while (true) {
+            val currentList = matrix[matrix.size - 1]
+            if (currentList.all { it == currentList[0] }) {
+                currentList.add(currentList[0])
+                break
+            }
+            val nextList: MutableList<Long> = mutableListOf()
+            for (i in 0..currentList.size - 2) {
+                nextList.add(currentList[i + 1] - currentList[i])
+            }
+            matrix.add(nextList)
+        }
+
+        for (j in matrix.size - 1 downTo 1) {
+            val lowerList = matrix[j]
+            val topList = matrix[j - 1]
+            topList.add(topList[topList.size - 1] + lowerList[lowerList.size - 1])
+        }
+        return list[list.size - 1]
+    }
+
+    fun part1() {
+        var sum = 0L
+        val strings: List<String> = File("src/main/resources/2023_day_9_input.txt").bufferedReader().readLines()
+        strings.forEach {
+            val nextResult: Long = calculate(it)
+            sum += nextResult
+        }
+
+        println(sum)
+    }
+
+//    fun part2() {
+//        val strings: List<String> = File("src/main/resources/2023_day_8_input.txt").bufferedReader().readLines()
+//
+//    }
+
+    part1()
+//    part2()
+
+}
+
+
+
+
