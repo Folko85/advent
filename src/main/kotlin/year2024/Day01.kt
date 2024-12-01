21219eb3ba5cf41824352d8fb1ea62e5365b7c58
GutorovMikhail <mgutorov@phoenixit.ru>
2024-12-02 00:58:09 +0300

    2024. День 1. Ищем главного историка. Часть вторая

--- src/main/kotlin/year2024/Day01.kt
+++ src/main/kotlin/year2024/Day01.kt
@@ -1,6 +1,7 @@
 package year2024
 
 import java.io.File
+import java.math.BigInteger
 import kotlin.math.abs
 
 fun main() {
@@ -23,9 +24,29 @@ fun main() {
             sum += abs(sortedFirst[i] - sortedSecond[i])
         }
         println(sum)
+    }
 
+    fun part2() {
+        val strings: List<String> = File("src/main/resources/year2024/day1_input.txt").bufferedReader().readLines()
+        val firstList: MutableList<BigInteger> = mutableListOf()
+        val secondMap: MutableMap<BigInteger, BigInteger> = mutableMapOf()
+        strings.forEach {
+            run {
+                val numbers: List<String> = it.split("\\s+".toRegex())
+                firstList.add(numbers[0].trim().toBigInteger())
+                secondMap.merge(numbers[1].trim().toBigInteger(), BigInteger.ONE) { new, old -> new + old }
+            }
+        }
+
+        var sum = BigInteger.ZERO
+        firstList.forEach {
+            val elementCount: BigInteger = secondMap[it] ?: BigInteger.ZERO
+            sum = sum.add(it.multiply(elementCount))
+        }
+        println(sum)
     }
 
 
     part1()
+    part2()
 }
