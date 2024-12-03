abeca1896790a5a505fdca7662c93dccd7b8c70d
GutorovMikhail <mgutorov@phoenixit.ru>
2024-12-03 16:36:11 +0300

    2024. День 2. Часть 2. Стырил из интернета более красивое решение.

--- src/main/kotlin/year2024/Day02.kt
+++ src/main/kotlin/year2024/Day02.kt
@@ -4,44 +4,38 @@ import java.io.File
 import kotlin.math.abs
 
 fun main() {
-
-    fun part1() {
-        val strings: List<String> = File("src/main/resources/year2024/day2_input.txt").bufferedReader().readLines()
-        var sum = 0
-        strings.forEach {
-            if (checkString(it)) {
-                sum++;
-            }
-        }
-        println(sum)
-    }
-
     part1()
+    part2()
 
 }
 
-fun checkString(string: String): Boolean {
-    val numbers: List<Int> = string.split("\\s+".toRegex()).map { it.trim().toInt() }
-    var increase = false
-    var decrease = false
-    for (i in numbers.indices) {
-        if (i == 0) {
-            continue
-        }
-        if (numbers[i] > numbers[i - 1]) {
-            increase = true
-        }
-        if (numbers[i] < numbers[i - 1]) {
-            decrease = true
-        }
-        if (increase && decrease) {
-            return false
-        }
-        val diff = abs(numbers[i] - numbers[i - 1])
-        if (diff < 1 || diff > 3) {
-            return false
+fun part1() {
+    val input: List<String> = File("src/main/resources/year2024/day2_input.txt").bufferedReader().readLines()
+    println(input.map { it.split(" ").map(String::toInt) }.count { isSafe(it) })
+}
+
+fun part2() {
+    val input: List<String> = File("src/main/resources/year2024/day2_input.txt").bufferedReader().readLines()
+    println(input.map { it.split(" ").map(String::toInt) }.count { isSafeWithDampener(it) })
+}
+
+fun isSafe(report: List<Int>): Boolean {
+    val isIncreasing = report.zipWithNext().all { it.second > it.first }
+    val isDecreasing = report.zipWithNext().all { it.second < it.first }
+    val differencesOk = report.zipWithNext().all { (a, b) -> (1..3).contains(abs(b - a)) }
+    return (isIncreasing || isDecreasing) && differencesOk
+}
+
+fun isSafeWithDampener(report: List<Int>): Boolean {
+    if (isSafe(report)) {
+        return true
+    }
+    for (i in report.indices) {
+        val modifiedReport = report.toMutableList().apply { removeAt(i) }
+        if (isSafe(modifiedReport)) {
+            return true
         }
     }
-    return true
-
+    return false
 }
+
