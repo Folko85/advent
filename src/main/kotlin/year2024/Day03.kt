b6a11cf2dcf3f191049b1525c1c235916245467d
GutorovMikhail <mgutorov@phoenixit.ru>
2024-12-03 23:38:14 +0300

    2024. День 3. Часть 2. Обдумай эти более сложные регулярки

--- src/main/kotlin/year2024/Day03.kt
+++ src/main/kotlin/year2024/Day03.kt
@@ -21,8 +21,37 @@ fun main() {
         println(sum)
     }
 
+    fun part2() {
+        val multiply: Regex = "mul\\((\\d{1,3}),(\\d{1,3}\\))|do\\(\\)|don't\\(\\)".toRegex()
+        val digits: Regex = "\\d+".toRegex()
+        val input: List<String> = File("src/main/resources/year2024/day3_input.txt").bufferedReader().readLines()
+        var sum = 0L
+        var enabled = true
+        input.forEach { string ->
+            multiply.findAll(string).forEach { matchResult ->
+                run {
+                    if (matchResult.value == "do()"){
+                        enabled = true
+                    } else if (matchResult.value == "don't()"){
+                        enabled = false
+                    } else {
+                        val multiplies: List<String> = matchResult.value.split(",".toRegex())
+                        val numbers: List<Long> = multiplies.map { digits.find(it)!!.value.toLong() }
+                        if (enabled){
+                            sum += numbers[0] * numbers[1]
+                        }
+
+                    }
+
+                }
+            }
+        }
+        println(sum)
+    }
+
 
     part1()
+    part2()
 
 }
 
