52fb866eba7ecd02be4d6b38d6d24782dbb91d06
GutorovMikhail <mgutorov@phoenixit.ru>
2024-12-09 23:48:47 +0300

    2024. День 8. Часть 2. Резонансная коллинеарность усиливается

--- src/main/kotlin/year2024/Day08.kt
+++ src/main/kotlin/year2024/Day08.kt
@@ -17,11 +17,33 @@ fun main() {
         return result
     }
 
-    fun part1() {
+    fun advancedFindAntiNodes(nodes: MutableList<Pair<Int, Int>>, size: Int): MutableSet<Pair<Int, Int>> {
+        val result: MutableSet<Pair<Int, Int>> = mutableSetOf()
+        for (i in nodes.indices) {
+            val firstPair = nodes[i]
+            result.add(firstPair)
+            for (j in i + 1..<nodes.size) {
+                val secondPair = nodes[j]
+                result.add(secondPair)
+                val shiftLeftPair = Pair(firstPair.first - secondPair.first, firstPair.second - secondPair.second)
+                for (k in 1..size) {
+                    result.add(Pair(firstPair.first + k * shiftLeftPair.first, firstPair.second + k * shiftLeftPair.second))
+                }
+                val shiftRightPair = Pair(secondPair.first - firstPair.first, secondPair.second - firstPair.second)
+                for (l in 1..size) {
+                    result.add(Pair(secondPair.first + l * shiftRightPair.first, secondPair.second + l * shiftRightPair.second))
+                }
+            }
+        }
+        return result
+    }
+
+    fun part1(isAdvanced: Boolean) {
         val nodes: MutableMap<String, MutableList<Pair<Int, Int>>> = mutableMapOf()
         val strings: List<String> = File("src/main/resources/year2024/day8_input.txt").bufferedReader().readLines()
         val rows: Int = strings.size
         val cols: Int = strings[0].length
+        val size = if (rows > cols) rows else cols
         val array: Array<Array<String>> = Array(rows) { Array(cols) { "" } }
         for (i in 0..<rows) {
             val row: String = strings[i]
@@ -37,16 +59,26 @@ fun main() {
         val antiNodes: MutableSet<Pair<Int, Int>> = mutableSetOf()
 
         nodes.values.forEach {
-            antiNodes.addAll(findAntiNodes(it))
+            if (isAdvanced) {
+                antiNodes.addAll(advancedFindAntiNodes(it, size * 2))
+            } else {
+                antiNodes.addAll(findAntiNodes(it))
+            }
+
         }
 
         val sum = antiNodes.count { it.first >= 0 && it.first < array.size && it.second >= 0 && it.second < array[0].size }
         println(sum)
     }
 
+    fun part2() {
+        part1(true)
+    }
 
 
-    part1()
+
+    part1(false)
+    part2()
 
 }
 
