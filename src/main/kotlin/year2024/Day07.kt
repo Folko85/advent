f68630fb7cde3970601dcf468693a45292f58a17
GutorovMikhail <mgutorov@phoenixit.ru>
2024-12-09 18:11:33 +0300

    2024. День 7. Часть 2. Ремонт верёвочного моста тупым перебором с конкатенацией

--- src/main/kotlin/year2024/Day07.kt
+++ src/main/kotlin/year2024/Day07.kt
@@ -16,6 +16,32 @@ fun main() {
         return false
     }
 
+    fun advancedCheckExpression(
+        expectedResult: BigInteger,
+        actualResult: BigInteger,
+        parts: List<BigInteger>,
+        iteration: Int
+    ): Boolean {
+        if (iteration == parts.size || expectedResult < actualResult) return expectedResult == actualResult
+        if (advancedCheckExpression(expectedResult, actualResult + parts[iteration], parts, iteration + 1)) {
+            return true
+        }
+        if (advancedCheckExpression(expectedResult, actualResult * parts[iteration], parts, iteration + 1)) {
+            return true
+        }
+        if (advancedCheckExpression(
+                expectedResult,
+                BigInteger(actualResult.toString() + parts[iteration].toString()),
+                parts,
+                iteration + 1
+            )
+        ) {
+            return true
+        }
+
+        return false
+    }
+
 
     fun part1() {
         val strings: List<String> = File("src/main/resources/year2024/day7_input.txt").bufferedReader().readLines()
@@ -29,8 +55,21 @@ fun main() {
         println(sum)
     }
 
+    fun part2() {
+        val strings: List<String> = File("src/main/resources/year2024/day7_input.txt").bufferedReader().readLines()
+        var sum = BigInteger.ZERO
+        strings.forEach { string ->
+            val splitString: List<String> = string.split(":".toRegex())
+            val result: BigInteger = splitString[0].trim().toBigInteger()
+            val parts: List<BigInteger> = splitString[1].trim().split("\\s+".toRegex()).map { it.trim().toBigInteger() }
+            sum = if (advancedCheckExpression(result, parts[0], parts, 1)) sum.add(result) else sum.add(BigInteger.ZERO)
+        }
+        println(sum)
+    }
+
 
     part1()
+    part2()
 
 }
 
