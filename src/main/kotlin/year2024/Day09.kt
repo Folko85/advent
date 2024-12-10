9945b51a00cd3dc29ce41a4c2947926feebf47b6
GutorovMikhail <mgutorov@phoenixit.ru>
2024-12-11 02:27:18 +0300

    2024. День 9. Часть 2. Дефрагментируем диск не разбивая файлы.

--- src/main/kotlin/year2024/Day09.kt
+++ src/main/kotlin/year2024/Day09.kt
@@ -31,5 +31,66 @@ fun main() {
         println(sum)
     }
 
+    fun findIndex(spaceCount: Int, spacesMap: MutableMap<Int, Int>): Int {
+        val sortedMap = spacesMap.toSortedMap()
+        for (entry in sortedMap.entries) {
+            if (entry.value >= spaceCount) {
+                return entry.key
+            }
+        }
+        return 0
+    }
+
+    fun part2() {
+        val string: String = File("src/main/resources/year2024/day9_input.txt").bufferedReader().readLine()!!
+
+        val all: MutableList<Int> = string.map { it.digitToInt() }.toMutableList()
+        val spacesMap: MutableMap<Int, Int> = mutableMapOf()
+        var key = 0
+        for (a in all.indices) {
+            if (a % 2 != 0) {
+                spacesMap[key] = all[a]
+            }
+            key += all[a]
+
+        }
+
+        val parsed: MutableList<Int?> =
+            string.flatMapIndexed { index, c -> List(c.digitToInt()) { if (index % 2 == 0) index / 2 else null } }.toMutableList()
+
+        for (i in all.size - 1 downTo 0 step 2) {
+            val length = all[i]
+            for (j in 1..<i step 2) {
+                val spaceCount = all[j]
+                if (spaceCount >= length) {
+                    for (index in parsed.indices) {
+                        if (parsed[index] == i / 2) {
+                            parsed[index] = null
+                        }
+                    }
+                    val parsedIndex: Int = findIndex(spaceCount, spacesMap)
+                    for (l in 0..<length) {
+                        parsed[parsedIndex + l] = i / 2
+                    }
+                    all[j] = spaceCount - length
+                    spacesMap.remove(parsedIndex)
+                    if (spaceCount - length > 0) {
+                        spacesMap[parsedIndex + length] = spaceCount - length
+                    }
+                    break
+                }
+            }
+        }
+
+        val filtered = parsed.map { it ?: 0 }
+
+        var sum = 0L
+        for (i in filtered.indices) {
+            sum += i.toLong() * filtered[i].toLong()
+        }
+        println(sum)
+    }
+
     part1()
+    part2()
 }
