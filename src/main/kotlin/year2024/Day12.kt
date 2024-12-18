55cc8722fe22943aaffa27191b5b1e6029925e76
GutorovMikhail <mgutorov@phoenixit.ru>
2024-12-18 23:33:20 +0300

    2024. День 12. Часть 2. Огораживаем растения и считаем углы.

--- src/main/kotlin/year2024/Day12.kt
+++ src/main/kotlin/year2024/Day12.kt
@@ -12,6 +12,18 @@ fun main() {
             Pair(1, 0)
         )
 
+    val directionsMap: Map<String, Pair<Int, Int>> =
+        mapOf(
+            "U" to Pair(0, -1),
+            "D" to Pair(0, 1),
+            "L" to Pair(-1, 0),
+            "R" to Pair(1, 0),
+            "UL" to Pair(-1, -1),
+            "UR" to Pair(1, -1),
+            "DL" to Pair(-1, 1),
+            "DR" to Pair(1, 1)
+        )
+
     fun findArea(
         i: Int,
         j: Int,
@@ -81,7 +93,90 @@ fun main() {
         println(sum)
     }
 
+    fun checkCorner(
+        horizontalPoint: Pair<Int, Int>,
+        centerPoint: Pair<Int, Int>,
+        verticalPoint: Pair<Int, Int>,
+        letter: String,
+        array: Array<Array<String>>
+    ): Long {
+        val centerOutside =
+            centerPoint.first < 0 || centerPoint.first >= array.size || centerPoint.second < 0 || centerPoint.second >= array[0].size
+        val horizontalOutside =
+            horizontalPoint.first < 0 || horizontalPoint.first >= array.size || horizontalPoint.second < 0 || horizontalPoint.second >= array[0].size
+        val verticalOutside =
+            verticalPoint.first < 0 || verticalPoint.first >= array.size || verticalPoint.second < 0 || verticalPoint.second >= array[0].size
+        if ((horizontalOutside || array[horizontalPoint.first][horizontalPoint.second] != letter)
+            && (verticalOutside || array[verticalPoint.first][verticalPoint.second] != letter)
+        ) {
+            return 1
+        }
+        if ((centerOutside || array[centerPoint.first][centerPoint.second] != letter)
+            && (!horizontalOutside && array[horizontalPoint.first][horizontalPoint.second] == letter)
+            && (!verticalOutside && array[verticalPoint.first][verticalPoint.second] == letter)
+        ) {
+            return 1
+        }
+        return 0
+    }
+
+    fun checkPoint(point: Pair<Int, Int>, letter: String, array: Array<Array<String>>): Long {
+        var result = 0L
+        val upPoint = Pair(point.first + directionsMap["U"]!!.first, point.second + directionsMap["U"]!!.second)
+        val downPoint = Pair(point.first + directionsMap["D"]!!.first, point.second + directionsMap["D"]!!.second)
+        val leftPoint = Pair(point.first + directionsMap["L"]!!.first, point.second + directionsMap["L"]!!.second)
+        val rightPoint = Pair(point.first + directionsMap["R"]!!.first, point.second + directionsMap["R"]!!.second)
+        val upLeftPoint = Pair(point.first + directionsMap["UL"]!!.first, point.second + directionsMap["UL"]!!.second)
+        val upRightPoint = Pair(point.first + directionsMap["UR"]!!.first, point.second + directionsMap["UR"]!!.second)
+        val downLeftPoint = Pair(point.first + directionsMap["DL"]!!.first, point.second + directionsMap["DL"]!!.second)
+        val downRightPoint = Pair(point.first + directionsMap["DR"]!!.first, point.second + directionsMap["DR"]!!.second)
+        result += checkCorner(leftPoint, upLeftPoint, upPoint, letter, array)
+        result += checkCorner(upPoint, upRightPoint, rightPoint, letter, array)
+        result += checkCorner(rightPoint, downRightPoint, downPoint, letter, array)
+        result += checkCorner(downPoint, downLeftPoint, leftPoint, letter, array)
+        return result
+    }
+
+    fun calculateCounters(plants: MutableSet<Pair<Int, Int>>, array: Array<Array<String>>): Long {
+        var corners = 0L
+        plants.forEach {
+            val letter = array[it.first][it.second]
+            corners += checkPoint(it, letter, array)
+        }
+        return corners * plants.size
+    }
+
+    fun part2() {
+        val checked: MutableList<Pair<Int, Int>> = mutableListOf()
+        val strings: List<String> = File("src/main/resources/year2024/day12_input.txt").bufferedReader().readLines()
+        val rows: Int = strings.size
+        val cols: Int = strings[0].length
+        val array: Array<Array<String>> = Array(rows) { Array(cols) { "" } }
+        val borderArray: Array<Array<Int>> = Array(rows) { Array(cols) { 0 } }
+        for (i in 0..<rows) {
+            val row: String = strings[i]
+            for (j in 0..<cols) {
+                array[i][j] = (row[j].toString())
+            }
+        }
+
+        var sum = 0L
+
+        for (i in 0..<rows) {
+            for (j in 0..<cols) {
+                if (!checked.contains(Pair(i, j))) {
+                    val plants: MutableSet<Pair<Int, Int>> = mutableSetOf()
+                    findArea(i, j, array, borderArray, plants)
+                    checked.addAll(plants)
+                    sum += calculateCounters(plants, array)
+                }
+            }
+        }
+        println(sum)
+    }
+
     part1()
+    part2()
 
 
 }
