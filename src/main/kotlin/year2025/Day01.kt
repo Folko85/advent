05880577af398633d63276cf2641ce23cf69453d
GutorovMikhail <mgutorov@phoenixit.ru>
2025-12-03 00:11:01 +0300

    2025. День 1. Часть 2. Секретный вход с круговым замком. Считаем все нули.

--- src/main/kotlin/year2025/Day01.kt
+++ src/main/kotlin/year2025/Day01.kt
@@ -2,7 +2,7 @@ package year2025
 
 import java.io.File
 
-fun main(){
+fun main() {
 
     fun part1() {
         var resultCounter = 0
@@ -15,20 +15,67 @@ fun main(){
             val direction = it[0]
             val moveValue = it.substring(1).toInt()
             val clearedMoveValue = moveValue % roundSize
-            if (direction == 'R'){
+            if (direction == 'R') {
                 val toEdgeValue = maxValue - currentValue
-                if (clearedMoveValue <= toEdgeValue){
+                if (clearedMoveValue <= toEdgeValue) {
                     currentValue += clearedMoveValue
                 } else {
                     currentValue -= (roundSize - clearedMoveValue)
                 }
 
-            } else if (direction == 'L'){
+            } else if (direction == 'L') {
                 val toEdgeValue = currentValue - minValue
-                if (clearedMoveValue <= toEdgeValue){
+                if (clearedMoveValue <= toEdgeValue) {
                     currentValue -= clearedMoveValue
                 } else {
                     currentValue += (roundSize - clearedMoveValue)
+                }
+            }
+            if (currentValue == 0) {
+                resultCounter++
+            }
+        }
+        println(resultCounter)
+
+    }
+
+    fun part2() {
+        var resultCounter = 0
+        val maxValue = 99
+        val minValue = 0
+        var currentValue = 50
+        val roundSize = 100
+        val commands: List<String> = File("src/main/resources/year2025/day1_input.txt").bufferedReader().readLines()
+        commands.forEach {
+            val direction = it[0]
+            val moveValue = it.substring(1).toInt()
+            val roundCount = moveValue / roundSize
+            resultCounter += roundCount
+            val clearedMoveValue = moveValue % roundSize
+            if (direction == 'R') {
+                val toEdgeValue = maxValue - currentValue
+                if (clearedMoveValue <= toEdgeValue) {
+                    currentValue += clearedMoveValue
+                } else {
+                    currentValue -= (roundSize - clearedMoveValue)
+                    if (currentValue != 0){
+                        resultCounter++
+                    }
+
+                }
+
+            } else if (direction == 'L') {
+                val toEdgeValue = currentValue - minValue
+                if (clearedMoveValue <= toEdgeValue) {
+                    currentValue -= clearedMoveValue
+                } else {
+                    val previousValue = currentValue
+                    currentValue += (roundSize - clearedMoveValue)
+                    if (currentValue != 0 && previousValue != 0){
+                        resultCounter++
+                    }
+
+
                 }
             }
             if (currentValue == 0){
@@ -40,4 +87,5 @@ fun main(){
     }
 
     part1()
+    part2()
 }
