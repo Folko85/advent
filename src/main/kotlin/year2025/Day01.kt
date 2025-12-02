b9ab916a8551c9da742853bf5d3634dfd726a332
GutorovMikhail <mgutorov@phoenixit.ru>
2025-12-02 23:32:00 +0300

    2025. День 1. Часть 1. Секретный вход с круговым замком.

---
+++ src/main/kotlin/year2025/Day01.kt
@@ -0,0 +1,43 @@
+package year2025
+
+import java.io.File
+
+fun main(){
+
+    fun part1() {
+        var resultCounter = 0
+        val maxValue = 99
+        val minValue = 0
+        var currentValue = 50
+        val roundSize = 100
+        val commands: List<String> = File("src/main/resources/year2025/day1_input.txt").bufferedReader().readLines()
+        commands.forEach {
+            val direction = it[0]
+            val moveValue = it.substring(1).toInt()
+            val clearedMoveValue = moveValue % roundSize
+            if (direction == 'R'){
+                val toEdgeValue = maxValue - currentValue
+                if (clearedMoveValue <= toEdgeValue){
+                    currentValue += clearedMoveValue
+                } else {
+                    currentValue -= (roundSize - clearedMoveValue)
+                }
+
+            } else if (direction == 'L'){
+                val toEdgeValue = currentValue - minValue
+                if (clearedMoveValue <= toEdgeValue){
+                    currentValue -= clearedMoveValue
+                } else {
+                    currentValue += (roundSize - clearedMoveValue)
+                }
+            }
+            if (currentValue == 0){
+                resultCounter++
+            }
+        }
+        println(resultCounter)
+
+    }
+
+    part1()
+}
